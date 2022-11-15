0ad9c1dc8620904305a33a4be3098f435fa4c769
lola martinhao <lola.martinhao@gmail.com>
2022-11-15 14:47:50 -0300

    Adding all Routes, Controller edits and Pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'tasks', to: 'tasks#index'
+
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  get 'taks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
